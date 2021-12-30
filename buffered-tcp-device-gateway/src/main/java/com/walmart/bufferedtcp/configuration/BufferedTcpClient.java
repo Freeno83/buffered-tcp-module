c3410403cfcda19d693ac8b72b8db5d063a462f1
Nicholas Robinson <60759127+Freeno83@users.noreply.github.com>
2021-12-30 12:17:35 -0600

    Add files via upload

--- buffered-tcp-device-gateway/src/main/java/com/walmart/bufferedtcp/configuration/BufferedTcpClient.java
+++ buffered-tcp-device-gateway/src/main/java/com/walmart/bufferedtcp/configuration/BufferedTcpClient.java
@@ -21,16 +21,23 @@ public class BufferedTcpClient{
     private String ipAddress;
     private int port;
     private BufferedReader input;
+    private Long lastConnectFailTime;
+    private Long lastNonNullMessageTime;
+    private int RECONNECT_TIME = 60000;
+    private int NO_MESSAGE_TIMEOUT = 30000;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public BufferedTcpClient(String ipAddress, int port) {
         this.ipAddress = ipAddress;
         this.port = port;
+        this.lastConnectFailTime = System.currentTimeMillis();
+        this.lastNonNullMessageTime = System.currentTimeMillis();
     }
 
     public void openSocket() {
         try{
             socket = new Socket(ipAddress, port);
+            socket.setKeepAlive(true);
             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             logger.info(String.format("Successfully opened connection to: (%s) port (%s)", ipAddress, port));
         } catch (Exception e) {
@@ -52,21 +59,46 @@ public class BufferedTcpClient{
 
         ArrayList<String> messages = new ArrayList();
         String line;
-        try {
-            while (input.ready() && (line = input.readLine()) != null)
-                messages.add(line);
-        }  catch (Exception e) {
-            logError(e,"Error reading message from: (%s) port (%s), cause: (%s)");
+        if(isConnected()) {
+            try {
+                while (input.ready() && (line = input.readLine()) != null)
+                    messages.add(line);
+            }  catch (Exception e) {
+                logError(e,"Error reading message from: (%s) port (%s), cause: (%s)");
+            }
+        }
+        if(messages.size() > 0) {
+            lastNonNullMessageTime = System.currentTimeMillis();
         }
         return String.join(",", messages);
     }
 
     public Boolean isConnected() {
-        return socket.isConnected();
-    }
+        Boolean connected = false;
+        try {
+            connected = socket.isConnected();
+        } catch (Exception e) {
+
+        }
+        if (!connected) {
+            if (System.currentTimeMillis() - lastConnectFailTime > RECONNECT_TIME) {
+                this.lastConnectFailTime = System.currentTimeMillis();
+                openSocket();
+            }
+        }
+        if (connected) {
+            if (System.currentTimeMillis() - lastNonNullMessageTime > NO_MESSAGE_TIMEOUT) {
+                logger.error(String.format("Resetting connection due to no messages received: (%s: %s)", ipAddress, port));
+                lastNonNullMessageTime = System.currentTimeMillis();
+                closeSocket();
+                openSocket();
+            }
+        }
+        return connected;
+        }
 
     public void logError(Exception e, String format) {
 
-        logger.error(String.format(format, ipAddress, port, e.getCause()));
+        logger.error(String.format(format, ipAddress, port, e));
     }
 }
